10ec82ebae4d1d82d14f03b58bf16fb829a626e7
hurzan.karina <kgurzan@gmail.com>
2025-03-31 15:47:29 +0000

    done

--- src/js/script.js
+++ src/js/script.js
@@ -46,6 +46,7 @@ async function renderCourses(page = 1) {
   paginatedCourses.forEach((course) => {
     const courseItem = document.createElement("li");
     courseItem.classList.add("course-item");
+    courseItem.innerHTML = '';
 
     courseItem.innerHTML = `   
             <img src="${course.photo}" alt="${course.name}">
